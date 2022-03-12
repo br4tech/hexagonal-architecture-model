6502ebd545182e790ff2e9ddc7043cd8fd3f8d99
Guilherme Henrique <guilherme.tr.silva@gmail.com>
2022-03-12 15:18:49 -0300

    update discount and contract layout to new version

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ContractsController < ApplicationController  
+class ContractsController < ApplicationController
   before_action :set_client, only: %i[index new edit]
   before_action :set_contract, only: %i[edit update]
 
@@ -26,16 +26,16 @@ class ContractsController < ApplicationController
   # POST /contracts.json
   def create
     @kind = PersonKind.all
-    @service = ContractService.new(contract_params)  
-    @service.create    
-    @contract = @service.contract    
-  
+    @service = ContractService.new(contract_params)
+    @service.create
+    @contract = @service.contract
+
     respond_to do |format|
       if @contract.save
         ScheduleWorker.new.perform(@contract.id)
         format.html { redirect_to combo_client_path(@contract.client_id, @contract.contract_combo, Company.categories.keys[@contract.category || 0]), notice: 'Contrato cadastrado com sucesso.' }
         format.json { render json: @contract, status: :created, location: @contract, message: 'Contrato cadastrado com sucesso' }
-      else        
+      else
         format.html { render :new }
         format.json { render json: @contract.errors.full_messages, status: :unprocessable_entity }
       end
@@ -44,9 +44,9 @@ class ContractsController < ApplicationController
 
   # PATCH/PUT /contracts/1
   # PATCH/PUT /contracts/1.json
-  def update  
+  def update
     @kind = PersonKind.all
-    @service = ContractService.new(contract_params, params[:id])    
+    @service = ContractService.new(contract_params, params[:id])
     @service.update
     @contract = @service.contract
 
@@ -54,8 +54,8 @@ class ContractsController < ApplicationController
       if @contract.valid?
         ScheduleWorker.new.perform(@contract.id)
         format.html { redirect_to combo_client_path(@contract.client_id, @contract.contract_combo, Company.categories.keys[@contract.category || 0]), notice: 'Contrato atualizado.' }
-        format.json { render :show, status: :ok, location: @contract }     
-      else              
+        format.json { render :show, status: :ok, location: @contract }
+      else
         format.html { render :show }
         format.json { render json: @contract.errors, status: :unprocessable_entity }
       end
@@ -73,25 +73,25 @@ class ContractsController < ApplicationController
   end
 
   private
-  
+
   # Use callbacks to shparamsare common setup or constraints between actions.
   def set_contract
     @contract = Contract.find(params[:id])
   end
 
   def set_client
-    return if params[:client_id].nil?    
-    @client = Client.find(params[:client_id])  
+    return if params[:client_id].nil?
+
+    @client = Client.find(params[:client_id])
   end
-  
+
   # Never trust parameters from the scary internet, only allow the white list through.
   def contract_params
     params.require(:contract)
-      .permit(:id, :contract_combo_id, :client_id, :category, :starts_at, :ends_at, :amount, :revenues_at, :due_at, :forfeit, :kind, :rescheduling_allowed, :parking, :parking_value, :car_license_plate,   
-        client_attributes: [:id, :name, :email, :kind, :document, :zipcode, :street, :number, :complement, :neighborhood, :city, :state,
-           doctors_attributes: [:id, :name, :email, :crm, :document, :phone, :_destroy]
-          ],        
-          discounts_attributes:     [:id, :amount, :starts_at, :ends_at, :_destroy],
-          attendances_attributes:   [:id, :office_id, :clinic_id, :frequency, :starts_at, :ends_at, :time_starts, :time_ends, :is_recurrent, :_destroy, weekdays: []])
+          .permit(:id, :contract_combo_id, :client_id, :category, :starts_at, :ends_at, :amount, :revenues_at, :due_at, :forfeit, :kind, :rescheduling_allowed, :parking, :parking_value, :car_license_plate,
+                  client_attributes: [:id, :name, :email, :kind, :document, :zipcode, :street, :number, :complement, :neighborhood, :city, :state,
+                                      { doctors_attributes: %i[id name email crm document phone _destroy] }],
+                  discounts_attributes: %i[id amount starts_at ends_at _destroy],
+                  attendances_attributes: [:id, :office_id, :clinic_id, :frequency, :starts_at, :ends_at, :time_starts, :time_ends, :is_recurrent, :_destroy, { weekdays: [] }])
   end
 end
